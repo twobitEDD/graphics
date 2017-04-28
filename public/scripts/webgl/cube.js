196565f40d01a4568f75e04086348640181b608d
jlchapa <l>
2017-04-28 16:21:58 -0700

    Get js for the experiment

---
+++ public/scripts/webgl/cube.js
@@ -0,0 +1,1 @@
+console.log("All the webgl code goes here!");
--- routes/webgl.js
+++ routes/webgl.js
@@ -2,18 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET webgl main page. */
-router.get('/', function(req, res, next) {
-  res.render('webgl/index', { title: 'blackpolygon - webgl' });
+router.get('/', function (req, res, next) {
+    res.render('webgl/index', { title: 'blackpolygon - webgl' });
 });
 
-router.get('/:experiment', function(req, res, next) {
+router.get('/:experiment', function (req, res, next) {
     console.log(req.params.experiment);
 
-    if(req.params.experiment === "cube"){
-        res.render('webgl/cube', { title: 'blackpolygon - cube' });
+    if (req.params.experiment === "cube") {
+        res.render('webgl/cube',
+            {
+                title: 'blackpolygon - cube',
+                scriptPath: '../scripts/webgl/cube.js'
+            });
     }
     next();
-  
+
 });
 
 module.exports = router;
